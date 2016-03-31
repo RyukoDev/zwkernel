bfe0e4796cff6e39ebdf145d5bf68a66d9ddc66c
iTechnoguy <xda@itechnoguy.com>
2016-03-31 16:06:55 -0700

    v2 testing

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Mar 29 19:47:58 MST 2016"
+#define UTS_VERSION "#12 SMP PREEMPT Thu Mar 31 15:51:06 MST 2016"
 #define LINUX_COMPILE_BY "issac"
 #define LINUX_COMPILE_HOST "UbuntuStore1"
-#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
+#define LINUX_COMPILER "gcc version 4.9.3 20141205 (prerelease) (crosstool-NG linaro-1.13.1+bzr2710 - Linaro GCC 2014.12) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.40custom-gc1d6cbf-dirty"
+#define UTS_RELEASE "3.10.40-v2-ondemand-g8075a0f-dirty"
